c1e64183241f3d56e1d798aa6917090d2f4f0468
Roel Arents <roel.arents@kadaster.nl>
2024-09-21 10:51:17 +0200

    use example.com
    PDOK-16956

--- internal/controller/util_test.go
+++ internal/controller/util_test.go
@@ -20,67 +20,67 @@ func Test_createIngressRuleMatchFromURL(t *testing.T) {
 	}{{
 		name: "no path",
 		args: args{
-			url:                     mustURLParse(t, "http://host.my"),
+			url:                     mustURLParse(t, "http://example.com"),
 			includeLocalhost:        false,
 			matchUnderscoreVersions: false,
 		},
-		want: "Host(`host.my`) && PathPrefix(``)",
+		want: "Host(`example.com`) && PathPrefix(``)",
 	}, {
 		name: "include localhost",
 		args: args{
-			url:                     mustURLParse(t, "http://host.my"),
+			url:                     mustURLParse(t, "http://example.com"),
 			includeLocalhost:        true,
 			matchUnderscoreVersions: false,
 		},
-		want: "(Host(`localhost`) || Host(`host.my`)) && PathPrefix(``)",
+		want: "(Host(`localhost`) || Host(`example.com`)) && PathPrefix(``)",
 	}, {
 		name: "no versions",
 		args: args{
-			url:                     mustURLParse(t, "http://host.my/some/path"),
+			url:                     mustURLParse(t, "http://example.com/some/path"),
 			includeLocalhost:        false,
 			matchUnderscoreVersions: false,
 		},
-		want: "Host(`host.my`) && PathPrefix(`/some/path`)",
+		want: "Host(`example.com`) && PathPrefix(`/some/path`)",
 	}, {
 		name: "v1, no replacing",
 		args: args{
-			url:                     mustURLParse(t, "http://host.my/some/path/v1"),
+			url:                     mustURLParse(t, "http://example.com/some/path/v1"),
 			includeLocalhost:        false,
 			matchUnderscoreVersions: false,
 		},
-		want: "Host(`host.my`) && PathPrefix(`/some/path/v1`)",
+		want: "Host(`example.com`) && PathPrefix(`/some/path/v1`)",
 	}, {
 		name: "v1",
 		args: args{
-			url:                     mustURLParse(t, "http://host.my/some/path/v1"),
+			url:                     mustURLParse(t, "http://example.com/some/path/v1"),
 			includeLocalhost:        false,
 			matchUnderscoreVersions: true,
 		},
-		want: "Host(`host.my`) && PathRegexp(`^/some/path/v1(_\\d+)?$`)",
+		want: "Host(`example.com`) && PathRegexp(`^/some/path/v1(_\\d+)?$`)",
 	}, {
 		name: "v2, followed by other segment",
 		args: args{
-			url:                     mustURLParse(t, "http://host.my/some/path/v2/foo"),
+			url:                     mustURLParse(t, "http://example.com/some/path/v2/foo"),
 			includeLocalhost:        false,
 			matchUnderscoreVersions: true,
 		},
-		want: "Host(`host.my`) && PathRegexp(`^/some/path/v2(_\\d+)?/foo$`)",
+		want: "Host(`example.com`) && PathRegexp(`^/some/path/v2(_\\d+)?/foo$`)",
 	}, {
 		name: "v1_3",
 		args: args{
-			url:                     mustURLParse(t, "http://host.my/some/path/v1_3"),
+			url:                     mustURLParse(t, "http://example.com/some/path/v1_3"),
 			includeLocalhost:        false,
 			matchUnderscoreVersions: true,
 		},
-		want: "Host(`host.my`) && PathRegexp(`^/some/path/v1(_3)?$`)",
+		want: "Host(`example.com`) && PathRegexp(`^/some/path/v1(_3)?$`)",
 	}, {
 		name: "combined (never happens)",
 		args: args{
-			url:                     mustURLParse(t, "http://host.my/some/path/v345/v666_78"),
+			url:                     mustURLParse(t, "http://example.com/some/path/v345/v666_78"),
 			includeLocalhost:        false,
 			matchUnderscoreVersions: true,
 		},
-		want: "Host(`host.my`) && PathRegexp(`^/some/path/v345(_\\d+)?/v666(_78)?$`)",
+		want: "Host(`example.com`) && PathRegexp(`^/some/path/v345(_\\d+)?/v666(_78)?$`)",
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
